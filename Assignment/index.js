3fe54564b500a3e3c0bb4894388a0f1d6d63b815
Zakkery Cales <zakkerycales@gmail.com>
2024-03-07 14:16:15 -0600

    completed

--- Assignment/index.js
+++ Assignment/index.js
@@ -1,3 +1,25 @@
 // let userPrompt = prompt("Please enter your name!");
 // alert(`Hello ${userPrompt}!`);
 
+
+let formHTML = document.getElementById("form");
+formHTML.addEventListener('submit', function(){
+    event.preventDefault()
+    let randomNumber = Math.floor(Math.random() * 200);
+    console.log(randomNumber)
+    
+    let subjectInputHtml = document.getElementById("subject")
+    let subjectInput = subjectInputHtml.value;
+    subjectInput += " #" + randomNumber;
+
+    let emailInputHtml = document.getElementById("email").value;
+
+    let bodyInputHtml = document.getElementById('body').value;
+
+    window.location.href = `mailto:zakkerycales@gmail.com?subject=${subjectInput}&cc=${emailInputHtml}&body=${bodyInputHtml}`;
+
+    
+    setTimeout(function(){
+        window.location.href = "./index.html", 5000
+    })
+})
